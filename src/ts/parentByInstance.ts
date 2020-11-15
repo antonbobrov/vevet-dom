9720e64f580b098e3668b7d10f990719eeb525f5
Anton Bobrov <anton.bobrov@hotmail.com>
2020-11-15 12:47:43 +0300

    Add a generic to define types

--- src/ts/parentByInstance.ts
+++ src/ts/parentByInstance.ts
@@ -1,7 +1,7 @@
 /**
  * Get the nearest parent element by instance.
  */
-export default function parentByInstance (
+export default function parentByInstance <T extends Element> (
     /**
      * The reference element (child)
      */
@@ -18,7 +18,7 @@ export default function parentByInstance (
      * Current level
      */
     currentLevel = 0,
-): Element | false {
+): T | false {
 
     // check nesting level
     if (currentLevel >= maxLevel) {
@@ -29,12 +29,19 @@ export default function parentByInstance (
 
     // if overlap, return the parent
     if (child.parentElement instanceof Element) {
-        if (child.parentElement instanceof instance) {
-            return child.parentElement;
+        if (child.parentElement instanceof fixInstance(instance)) {
+            return child.parentElement as any;
         }
         return parentByInstance(child.parentElement, instance, maxLevel, currentLevel);
     }
 
     return false;
 
+}
+
+/**
+ * @ignore
+ */
+function fixInstance (instance: any) {
+    return instance;
 }
